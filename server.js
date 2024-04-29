f7da8b42f92332f0df161c1d26e6d60b57f2d1af
AmirHosineEnsafi <amirmahdion1@gmail.com>
2024-04-29 14:23:34 +0330

    wrote the author part of the code in the server.js

--- server.js
+++ server.js
@@ -56,11 +56,60 @@ function objectIdvalidate(id){
 
 app.use(express.JSON())
 
+app.get('/authors' , async (req , res) => {
+    const authors = await Author.find()
+    res.send(JSON.stringify(authors , null , 2))
+})
+
+app.post('/authors' , async (req , res) => {
+    const  {error} = joiSchemaAuthor.validate(req.body)
+    if(error)return res.status(400).send(error.details[0].message)
+    const author = new Author({
+        name : req.body.name,
+        lastName : req.body.lastName,
+        age : req.body.age
+    })
+    const result = await author.save()
+    res.send(result)
+})
+
+app.get('/authors/:id' , async (req , res) => {
+    if(!objectIdvalidate(req.params.id))return res.status(400).send(`id must be valid id`)
+    const author = await Author.findById(req.body.id)
+    if(!author) return res.status(404),send(`author not found try with correct id`)
+    res.send(author)
+})
+
+const putJoiSchema = Joi.object({
+    name : Joi.string().min(3).max(15),
+    lastName : Joi.string().min(3).max(15),
+    age : Joi.number().min(12).max(100)
+})
+
+app.put('/authors/:id' , async (req, res) => {
+    if(!objectIdvalidate(req.params.id))return res.status(400).send(`id must be valid id`)
+    const author = await Author.findById(req.body.id)
+    if(!author) return res.status(404),send(`author not found try with correct id`)
+    const {error} = putJoiSchema.validate(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+    if(req.body.name) author.name = req.body.name
+    if(req.body.lastName) author.lastName = req.body.lastName
+    if(req.body.age) author.age = req.body.age
+    const result = await author.save()
+    res.send(result)
+})
+
+app.delete('/authors/:id' , async (req , res) => {
+    if(!objectIdvalidate(req.params.id)) return res.status(400).send(`id is not valid id please send the valid id`)
+    const author = Author.findByIdAndDelete(req.params.id) 
+    if(!author) return res.status(404).send(`id not found please check the id you send`)
+    res.send(`author deleted successfully and this is the author lastName : ${author.lastName}`)
+})
+
 app.get('/books' , async (req , res) => {
     const books = await Book.find().populate('author')
     res.send(JSON.stringify(books , null , 2))
 })
-
 
 
 app.post('books' , async (req, res) => {
